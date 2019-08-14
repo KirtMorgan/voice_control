59e1ec9de5e16b2b5ebd8c50f786033d18defc38
KirtMorgan <kirtmorgan@live.com>
2019-08-14 21:24:24 +0100

    First upload of voice control project

---
+++ main.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import os
+
+recognition = sr.Recognizer()
+default_mic = sr.Microphone()
+r2 = True
+
+
+def activate(phrase='hello'):
+    try:
+        with default_mic as activate_source:
+            recognition.adjust_for_ambient_noise(activate_source)
+            activate_audio = recognition.listen(activate_source)
+            try:
+                activate_output = recognition.recognize_google(activate_audio)
+            except:
+                Exception
+            if activate_output == phrase:
+                print('Listening')
+                return True
+            else:
+                print('Not Listening')
+                return False
+
+    except:
+        Exception
+
+
+while r2:
+    activate()
+    while activate:
+        with default_mic as source:
+            audio = recognition.listen(source)
+            try:
+                output = recognition.recognize_google(audio)
+            except: Exception
+        if output == 'shutdown computer':
+            os.system('shutdown /s /t 1')
+        elif output == 'restart computer':
+            os.system('shutdown /r /t 1')
+        else:
+            print(output)
